2214a1b21ec4bcf691fe55134fdfe2c0399a6df2
Maksim Levin <mmlevin@mail.ru>
2017-04-05 13:25:27 +0300

    Added files into repo.

---
+++ app/src/main/java/com/wmmaks/c500companion/C500Helper.java
@@ -0,0 +1,61 @@
+/* C500Companion - An open source, free addition for Ownice C500 head unit
+ * Copyright (C) 2017 Maksim M. Levin. Russia, Voronezh
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contacts:
+ *            email: mmlevin@mail.ru
+*/
+
+package com.wmmaks.c500companion;
+
+import android.content.Context;
+
+public class C500Helper {
+    public enum C500_MODES {
+        C500_UNINSTALL,
+        C500_NAVI,
+        C500_MUSIC,
+        C500_VIDEO,
+        C500_DVD,
+        C500_DVDBOX,
+        C500_ADAS,
+        C500_AVIN,
+        C500_BACKCARMAIN,
+        C500_RIGHT_CAMERA,
+        C500_AUX,
+        C500_RADIO,
+        C500_BACKCAR,
+        C500_CMMB,
+        C500_ATV,
+        C500_DVR,
+        C500_FRONT_CAMERA,
+        C500_IPOD,
+        C500_TSDAILER,
+        C500_VIEW360,
+        C500_SETTINGS,
+        C500_CARINFO,
+        C500_BT_CONNECT,
+        C500_BT_MUSIC,
+        C500_CAN_CAR_DEVICE,
+        C500_CAN_EXRADIO,
+        C500_CAN_EXCD,
+        C500_CAN_FORD,
+        C500_TPMS,
+        C500_TPMS_SETTINGS,
+        C500_OBD,
+        C500_WEATHER
+    };
+}
+
---
+++ app/src/main/java/com/wmmaks/c500companion/C500Service.java
@@ -0,0 +1,247 @@
+/* C500Companion - An open source, free addition for Ownice C500 head unit
+ * Copyright (C) 2017 Maksim M. Levin. Russia, Voronezh
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contacts:
+ *            email: mmlevin@mail.ru
+*/
+
+package com.wmmaks.c500companion;
+
+import android.app.IntentService;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.SystemClock;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.KeyEvent;
+
+public class C500Service extends IntentService {
+    private static final String LOG_TAG = "C500Companion";
+    private static final String PREFS_NAME = "C500Preferences";
+    private static final String PREFS_MODE = "C500Mode";
+
+    private static final String ACTION = "com.wmmaks.c500companion.ACTION";
+    private static final String CMD = "CMD";
+
+    private static final int CMD_MODE_ENTER_SLEEP = 1;
+    private static final int CMD_MODE_RESTORE_SLEEP = 2;
+    private static final int CMD_MODE_CHANGE = 3;
+    private static final int CMD_MODE_SEEK_UP = 4;
+    private static final int CMD_MODE_SEEK_DOWN = 5;
+
+    public static final String POWERAMP_API_COMMAND = "com.maxmpz.audioplayer.API_COMMAND";
+    public static final String POWERAMP_PACKAGE_NAME = "com.maxmpz.audioplayer";
+    public static final String POWERAMP_API_COMMAND_CMD = "cmd";
+    public static final int POWERAMP_API_COMMAND_TOGGLE_PLAY_PAUSE = 1;
+    public static final int POWERAMP_API_COMMAND_PAUSE = 2;
+    public static final int POWERAMP_API_COMMAND_RESUME = 3;
+    public static final int POWERAMP_API_COMMAND_NEXT = 4;
+    public static final int POWERAMP_API_COMMAND_PREVIOUS = 5;
+
+    public static final String C500_BROADCAST_ACTION_COMMAND = "com.globalconstant.BROADCAST_SEND_CMD";
+    public static final String C500_COMMAND_DOMAIN = "domain";
+    public static final String C500_COMMAND_DOMAIN_DEVICE = "device";
+    public static final String C500_EXTRA_DEVICE_NAME = "device_name";
+    public static final String C500_EXTRA_ACTION = "action";
+    public static final String C500_EXTRA_ACTION_OPEN = "open";
+    public static final String C500_EXTRA_DEVICE_NAME_MUSIC = "music";
+    public static final String C500_EXTRA_DEVICE_NAME_RADIO = "radio";
+    public static final String C500_EXTRA_DEVICE_NAME_AVIN = "avin";
+
+    private SharedPreferences settings;
+    private int mMode;
+    private C500Helper.C500_MODES mModes [] = {
+            C500Helper.C500_MODES.C500_RADIO,
+            C500Helper.C500_MODES.C500_MUSIC,
+            C500Helper.C500_MODES.C500_AVIN
+    };
+
+    public C500Service() {
+        super("C500Service");
+    }
+
+    public static void KeyPressDownAndUp(int key,Context context){
+        long eventtime = SystemClock.uptimeMillis() - 1;
+
+        Intent downIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+        KeyEvent downEvent = new KeyEvent(eventtime, eventtime, KeyEvent.ACTION_DOWN, key, 0);
+        downIntent.putExtra(Intent.EXTRA_KEY_EVENT, downEvent);
+        context.sendOrderedBroadcast(downIntent, null);
+
+        eventtime++;
+        Intent upIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+        KeyEvent upEvent = new KeyEvent(eventtime, eventtime, KeyEvent.ACTION_UP, key, 0);
+        upIntent.putExtra(Intent.EXTRA_KEY_EVENT, upEvent);
+        context.sendOrderedBroadcast(upIntent, null);
+    }
+
+    public void openApplication(Context context, String packageName) {
+        PackageInfo pi;
+        try {
+            pi = context.getPackageManager().getPackageInfo(packageName, 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            pi = null;
+            e.printStackTrace();
+        }
+        if (pi != null) {
+            Intent resolveIntent = new Intent("android.intent.action.MAIN", null);
+            resolveIntent.setPackage(pi.packageName);
+            ResolveInfo ri = (ResolveInfo) context.getPackageManager().queryIntentActivities(resolveIntent, 0).iterator().next();
+            if (ri != null) {
+                packageName = ri.activityInfo.packageName;
+                String className = ri.activityInfo.name;
+                Intent intent = new Intent("android.intent.action.MAIN");
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setComponent(new ComponentName(packageName, className));
+                context.startActivity(intent);
+            }
+        }
+    }
+
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION.equals(action)) {
+                int param = intent.getIntExtra(CMD,0);
+                RestoreState();
+                SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+
+                boolean usePowerAmp = sharedPref.getBoolean(getString(R.string.prefPowerAmpUse),getResources().getBoolean(R.bool.prefPowerAmpUseDefault));
+                boolean usePowerAmpAPI = sharedPref.getBoolean(getString(R.string.prefPowerAmpUseApi),getResources().getBoolean(R.bool.prefPowerAmpUseApiDefault));
+                boolean pauseOnSleep = sharedPref.getBoolean(getString(R.string.prefPowerAmpPauseOnSleep),getResources().getBoolean(R.bool.prefPowerAmpPauseOnSleepDefault));
+                boolean playOnWakeup = sharedPref.getBoolean(getString(R.string.prefPowerAmpPlayOnWakeup),getResources().getBoolean(R.bool.prefPowerAmpPlayOnWakeupDefault));
+                boolean switchWithSeek = sharedPref.getBoolean(getString(R.string.prefPowerAmpSwitchWithSeek),getResources().getBoolean(R.bool.prefPowerAmpSwitchWithSeekDefault));
+
+                switch (param) {
+                    case CMD_MODE_ENTER_SLEEP:
+                        Log.d(LOG_TAG,"Received ENTER_SLEEP");
+                        if (pauseOnSleep) {
+                            if (usePowerAmp && usePowerAmpAPI) {
+                                intent = new Intent(POWERAMP_API_COMMAND);
+                                intent.setPackage(POWERAMP_PACKAGE_NAME);
+                                intent.putExtra(POWERAMP_API_COMMAND_CMD, POWERAMP_API_COMMAND_PAUSE);
+                                startService(intent);
+                            } else {
+                                KeyPressDownAndUp(KeyEvent.KEYCODE_MEDIA_PAUSE, this);
+                            }
+                        }
+                        break;
+                    case CMD_MODE_RESTORE_SLEEP:
+                        Log.d(LOG_TAG,"Received RESTORE_SLEEP");
+                        if (mModes[mMode] == C500Helper.C500_MODES.C500_MUSIC) {
+                            SetMode(mModes[mMode], usePowerAmp);
+                            if (playOnWakeup) {
+                                if (usePowerAmp && usePowerAmpAPI) {
+                                    intent = new Intent(POWERAMP_API_COMMAND);
+                                    intent.setPackage(POWERAMP_PACKAGE_NAME);
+                                    intent.putExtra(POWERAMP_API_COMMAND_CMD, POWERAMP_API_COMMAND_RESUME);
+                                    startService(intent);
+                                } else {
+                                    KeyPressDownAndUp(KeyEvent.KEYCODE_MEDIA_PLAY, this);
+                                }
+                            }
+                        }
+                        break;
+                    case CMD_MODE_CHANGE:
+                        Log.d(LOG_TAG,"Received mode change");
+                        if (++mMode >= mModes.length) {mMode = 0;}
+                        SetMode(mModes[mMode],usePowerAmp);
+                        break;
+                    case CMD_MODE_SEEK_DOWN:
+                        Log.d(LOG_TAG,"Received SEEK_DOWN");
+                        if (switchWithSeek) {
+                            if (usePowerAmp && usePowerAmpAPI) {
+                                intent = new Intent(POWERAMP_API_COMMAND);
+                                intent.setPackage(POWERAMP_PACKAGE_NAME);
+                                intent.putExtra(POWERAMP_API_COMMAND_CMD, POWERAMP_API_COMMAND_PREVIOUS);
+                                startService(intent);
+                            } else {
+                                KeyPressDownAndUp(KeyEvent.KEYCODE_MEDIA_PREVIOUS, this);
+                            }
+                        }
+                        break;
+                    case CMD_MODE_SEEK_UP:
+                        Log.d(LOG_TAG,"Received SEEK_UP");
+                        if (switchWithSeek) {
+                            if (usePowerAmp && usePowerAmpAPI) {
+                                intent = new Intent(POWERAMP_API_COMMAND);
+                                intent.setPackage(POWERAMP_PACKAGE_NAME);
+                                intent.putExtra(POWERAMP_API_COMMAND_CMD, POWERAMP_API_COMMAND_NEXT);
+                                startService(intent);
+                            } else {
+                                KeyPressDownAndUp(KeyEvent.KEYCODE_MEDIA_NEXT, this);
+                            }
+                        }
+                        break;
+                }
+                SaveState();
+            } else {
+                Log.d(LOG_TAG,"Received: " + action);
+            }
+        }
+    }
+
+    private void SetMode(C500Helper.C500_MODES mode, boolean usePowerAmp) {
+        Log.d(LOG_TAG,"Switching to " + mode.name());
+        Intent intent;
+        switch (mode) {
+            case C500_MUSIC:
+                if (usePowerAmp) {
+                    openApplication(this, POWERAMP_PACKAGE_NAME);
+                } else {
+                    intent = new Intent (C500_BROADCAST_ACTION_COMMAND);
+                    intent.putExtra(C500_COMMAND_DOMAIN, C500_COMMAND_DOMAIN_DEVICE);
+                    intent.putExtra(C500_EXTRA_DEVICE_NAME, C500_EXTRA_DEVICE_NAME_MUSIC);
+                    intent.putExtra(C500_EXTRA_ACTION, C500_EXTRA_ACTION_OPEN);
+                    sendBroadcast(intent);
+                }
+                break;
+            case C500_RADIO:
+                intent = new Intent (C500_BROADCAST_ACTION_COMMAND);
+                intent.putExtra(C500_COMMAND_DOMAIN, C500_COMMAND_DOMAIN_DEVICE);
+                intent.putExtra(C500_EXTRA_DEVICE_NAME, C500_EXTRA_DEVICE_NAME_RADIO);
+                intent.putExtra(C500_EXTRA_ACTION, C500_EXTRA_ACTION_OPEN);
+                sendBroadcast(intent);
+                break;
+            case C500_AVIN:
+                intent = new Intent (C500_BROADCAST_ACTION_COMMAND);
+                intent.putExtra(C500_COMMAND_DOMAIN, C500_COMMAND_DOMAIN_DEVICE);
+                intent.putExtra(C500_EXTRA_DEVICE_NAME, C500_EXTRA_DEVICE_NAME_AVIN);
+                intent.putExtra(C500_EXTRA_ACTION, C500_EXTRA_ACTION_OPEN);
+                sendBroadcast(intent);
+                break;
+        }
+    }
+
+    void RestoreState () {
+        settings = getSharedPreferences(PREFS_NAME,MODE_PRIVATE);
+        mMode = settings.getInt(PREFS_MODE,0);
+    }
+
+    void SaveState () {
+        if (settings == null) settings = getSharedPreferences(PREFS_NAME,MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt(PREFS_MODE,mMode);
+        editor.apply();
+    }
+}
